8ee13582996677c1af5dd674ca53e0eb832d0350
jian lee <372248701@qq.com>
2019-04-30 16:17:30 +0800

    save

--- myweb/service/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-app.use(express.static(path.resolve('../')+ '/dist'))
-
-app.listen(8000,()=>{
-    console.log("服务已启动~")
-});
